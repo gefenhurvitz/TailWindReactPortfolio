d12d0adc7ceaef3e6299cf96e1a628e8a50805eb
Gefen hurvitz <gefenhurvitz@Gefens-MacBook-Air.local>
2023-06-10 12:50:48 +0300

    working on Navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div>
+      <Navbar/>
       <h1 className="text-3xl font-bold">First TailWind design</h1>
 
     </div>
