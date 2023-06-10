22534e25c52cb12d09b7067ff631b7e09136def8
Gefen hurvitz <gefenhurvitz@Gefens-MacBook-Air.local>
2023-06-10 12:58:58 +0300

    add hamburger and mobile menu to the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,43 @@
-import React from 'react'
-import Logo from '../assets/logo1.png'
-// import {FaBars , FaTimes} from 'react-icons/fa'
+import React from "react";
+import Logo from "../assets/logo1.png";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 const Navbar = () => {
   return (
-    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
-        <div>
-            <img src={Logo} alt="Logo image " style={{width:'50px'}} />
-        </div>
-        <div>
-            <ul>
-                <li>Home</li>
-            </ul>
-        </div>
+    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
+      <div>
+        <img src={Logo} alt="Logo" style={{ width: "50px" }} />
+      </div>
 
+      {/* menu */}
+      <div>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Skills</li>
+          <li>Work</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+
+      {/* Hamburger */}
+      <div className="hidden">
+        <FaBars />
+      </div>
+
+      {/* Mobile menu */}
+      <ul className="hidden">
+        <li>Home</li>
+        <li>About</li>
+        <li>Skills</li>
+        <li>Work</li>
+        <li>Contact</li>
+      </ul>
+
+      {/* Social icons */}
+      <div></div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
