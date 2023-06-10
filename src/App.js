a5ca8c9e4b3199ef601e692f3e4a01edb27e9918
Gefen hurvitz <gefenhurvitz@Gefens-MacBook-Air.local>
2023-06-10 12:32:58 +0300

    add folders assets and components + adding photos

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div>
-      <h1 className="text-3xl">First TailWind design</h1>
+      <h1 className="text-3xl font-bold">First TailWind design</h1>
 
     </div>
   );
