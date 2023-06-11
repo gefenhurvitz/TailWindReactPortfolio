ff7ce2057b18e8de5fcfe5e88fdbff5585bf789a
Gefen hurvitz <gefenhurvitz@Gefens-MacBook-Air.local>
2023-06-11 17:16:48 +0300

    add home page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import Home from './components/Home';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div>
       <Navbar/>
-      <h1 className="text-3xl font-bold">First TailWind design</h1>
+      <Home/>
 
     </div>
   );
